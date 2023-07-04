f03415114f66a19c2d007dfcbce3411044a11738
孫　鵬釗 <houjou.son@micro-wave.net>
2023-07-04 18:34:53 +0900

    ♻️ コンポーネントを作成

---
+++ components/NextImage.jsx
@@ -0,0 +1,19 @@
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+
+export function NextImage() {
+  return (
+    <>
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+    </>
+  );
+}
