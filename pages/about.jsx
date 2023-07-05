fc9de5f3ef2bb42695f23e16bfa156a2bf49be59
孫　鵬釗 <houjou.son@micro-wave.net>
2023-07-05 15:18:09 +0900

    ✏️ 文言修正、不要コード削除

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { DocsGrid } from "@/components/DocsGrid";
 import { NextImage } from "@/components/NextImage";
 import { Header } from "@/components/Header";
 import { ABOUT_PAGE_TITLE } from "@/utils/path";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
