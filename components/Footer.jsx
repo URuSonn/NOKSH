334383b1f389a882fe7f4544bdddc2bd60e9ec64
孫　鵬釗 <houjou.son@micro-wave.net>
2023-07-07 10:22:53 +0900

    ✨ フッター、ヘッダー修正

---
+++ components/Footer.jsx
@@ -0,0 +1,23 @@
+const { Paper, Typography } = require("@mui/material");
+
+export function Footer() {
+  return (
+    <>
+      <Paper
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "80px",
+          height: "70px",
+          width: "100%",
+          background:
+            "linear-gradient(56deg, rgba(189,34,195,1) 6%, rgba(34,193,195,1) 51%, rgba(163,189,107,1) 78%, rgba(253,187,45,1) 100%);",
+        }}>
+        <Typography sx={{ fontSize: "30px", fontFamily: "cursive" }}>
+          Footer
+        </Typography>
+      </Paper>
+    </>
+  );
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,21 @@
+import { Typography } from "@mui/material";
 import styles from "../styles/Home.module.css";
 import { ContactLinks } from "./ContactLinks";
 import classes from "./Header.module.css";
 import { UsingFunction } from "./UsingFunction";
 
 export function Header(props) {
-  console.log("props", props);
+  console.log("props", props.color);
   const title = props.title;
+  const color = props.color;
   return (
     <>
       <div className={styles.description}>
-        <h1>{title}</h1>
-
+        <Typography
+          component="h1"
+          style={{ color: `${color}`, fontWeight: "bold" }}>
+          {title}
+        </Typography>
         <UsingFunction />
       </div>
       <ContactLinks />
